c466f833266ae597541cc91e26d4ac1098837474
Garima Gupta <ggupta2@uncc.edu>
2026-09-07 18:22:46 -0400

    Export the public BriefBot workflow API

--- src/briefbot/__init__.py
+++ src/briefbot/__init__.py
@@ -1,5 +1,26 @@
 """Local-first project brief automation."""
 
+from .input import brief_from_dict, load_brief
+from .models import ActionItem, BriefError, BriefInput, RiskItem
+from .output import write_output
+from .planning import ActionState, Brief, PlannedAction, build_brief
+from .report import brief_to_dict, format_brief
+
 __version__ = "0.1.0"
 
-__all__ = ["__version__"]
+__all__ = [
+    "ActionItem",
+    "ActionState",
+    "Brief",
+    "BriefError",
+    "BriefInput",
+    "PlannedAction",
+    "RiskItem",
+    "brief_from_dict",
+    "brief_to_dict",
+    "build_brief",
+    "format_brief",
+    "load_brief",
+    "write_output",
+    "__version__",
+]
