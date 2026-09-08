c02b8b214f65791c1042e394aec382e244e1239b
Garima Gupta <ggupta2@uncc.edu>
2026-09-08 18:36:15 -0400

    Export readiness APIs and sync runtime version

--- src/briefbot/__init__.py
+++ src/briefbot/__init__.py
@@ -4,9 +4,18 @@ from .input import brief_from_dict, load_brief
 from .models import ActionItem, BriefError, BriefInput, RiskItem
 from .output import write_output
 from .planning import ActionState, Brief, PlannedAction, build_brief
-from .report import brief_to_dict, format_brief
+from .policy import load_policy, policy_from_dict
+from .readiness import (
+    ReadinessCode,
+    ReadinessFinding,
+    ReadinessPolicy,
+    ReadinessResult,
+    ReadinessSeverity,
+    assess_readiness,
+)
+from .report import brief_to_dict, format_brief, format_policy, format_readiness
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 __all__ = [
     "ActionItem",
@@ -16,11 +25,21 @@ __all__ = [
     "BriefInput",
     "PlannedAction",
     "RiskItem",
+    "ReadinessCode",
+    "ReadinessFinding",
+    "ReadinessPolicy",
+    "ReadinessResult",
+    "ReadinessSeverity",
+    "assess_readiness",
     "brief_from_dict",
     "brief_to_dict",
     "build_brief",
     "format_brief",
+    "format_policy",
+    "format_readiness",
     "load_brief",
+    "load_policy",
+    "policy_from_dict",
     "write_output",
     "__version__",
 ]
